284106a2754a980c1f2576a4b1bd8bd669c57c0f
AlaaEzat <83536985+AlaaEzat-1@users.noreply.github.com>
2026-05-17 22:21:42 +0300

    Improved design

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-// 1. تفاعل زر إلقاء التحية
 const helloButton = document.getElementById('helloBtn');
 
 helloButton.addEventListener('click', function() {
     alert('أهلاً بك في موقعي الشخصي! سعيد جداً بزيارتك.');
 });
 
-// 2. تحديث السنة الحالية تلقائياً في التذييل (Footer)
 const yearSpan = document.getElementById('year');
 const currentYear = new Date().getFullYear();
 
